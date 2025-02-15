d299eb561175a44c1408f7a1a24fb15ce24f0622
Shone Renjan <shonerenjan@Mac-mini-2.local>
2025-02-15 12:26:54 -0600

    calender comitted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CalenderPage from './pages/CalenderPage';
 
 function App() {
 
@@ -12,6 +13,8 @@ function App() {
           <Route path="/" element={<h1>Home</h1>} />
           <Route path="/register" element={<h1>Register</h1>} />
           <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/calender" element={<CalenderPage />} />
+
           
 
         </Routes>
