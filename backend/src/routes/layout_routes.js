58e506a0b41e9f9c1e573d7d4c05e2d40e70fba7
Pravachan Patra <pravachanpatra@gmail.com>
2025-02-15 15:57:00 -0600

    format layout data & send request to backend

--- backend/src/routes/layout_routes.js
+++ backend/src/routes/layout_routes.js
@@ -1,19 +1,46 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
+// create app
 const app = express()
 app.use(express.json())
 app.use(cors())
-
 const layoutRouter = express.Router();
 
-/* Creates a layout object when user clicks save button */
+// import models
+import LayoutModel from "../models/Layout.js";
+
+
+/* Creates a layout object when user clicks save button 
+
+Example data recived with request:
+Connected to MongoDB
+layout-dimensions: {
+  "width": 640,
+  "height": 426
+}
+crop-areas: [
+  {
+    "cropType": "Corn",
+    "irrigation": "drip",
+    "fertilizerType": "nitrogen",
+    "fertilizerMethod": "broadcasting",
+    "width": 324,
+    "height": 189
+  }
+]
+Toal farm area is in meters, while crop area area is in square meters. 
+*/
 layoutRouter.post("/create-layout", async (req, res) => {
     try {
-        const { name, crop_areas, equipments, total_cost } = req.body;
 
-        res.status(201).json({ message: "Layout saved successfully", layout: newLayout });
+        const { dimensions, crops } = req.body;
+        console.log("layout-dimensions: " + JSON.stringify(dimensions, null, 2));
+        console.log("crop-areas: " + JSON.stringify(crops, null, 2));
+
+        const new_layout = new Lay
+
+        res.status(201).json({ message: "Layout saved successfully" }); //  layout: newLayout 
     } catch (error) {
         res.status(500).json({ message: "Error saving layout", error: error.message });
     }
