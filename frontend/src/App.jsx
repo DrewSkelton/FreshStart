a47048fe550b8fa9e39ecf99e6e60db731443640
DrewSkelton <drew.skelton@ou.edu>
2025-02-15 19:16:58 -0600

    create crop page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CalendarPage from './pages/CalendarPage';
+import CropPage from './pages/CropPage';
 
 function App() {
 
@@ -14,8 +15,7 @@ function App() {
           <Route path="/register" element={<h1>Register</h1>} />
           <Route path="/login" element={<h1>Login</h1>} />
           <Route path="/calendar" element={<CalendarPage />} />
-
-          
+          <Route path="/crop" element={<CropPage />} />
 
         </Routes>
       </div>
